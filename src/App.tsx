23d7246e71618838ecfdfae4888aa34adabb6eed
Ramazanov Djabrail <ramazanovd612@gmail.com>
2022-11-03 19:28:41 +0600

    я закончил

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,70 @@
-import React  from 'react';
+import React, {ReactElement, ReactNode, useState} from 'react';
 import './App.css';
+import Sapper from "./components/sapper/Sapper";
 
 function App() {
+
+  let randoNumber: number = 0
+
+  const createItems = () => {
+    const sappers = [];
+    randoNumber = (Math.floor(Math.random() * 36))
+    for (let i = 0; i < 36; i++) {
+      const sapper = {
+        hasItem: false,
+        clicked: false
+      }
+
+      if (i === randoNumber) {
+        sapper.hasItem = true
+      }
+      sappers.push(sapper)
+    }
+
+    return sappers
+  }
+
+  const [items, setItems] = useState(createItems())
+  const [showItems , setShowItems] =  useState(false)
+
+  let sapperList: React.ReactNode = null;
+
+
+  const resetSapper = () => {
+    randoNumber = (Math.floor(Math.random() * 36))
+    const itemsCopy = [...items]
+    let indexItems = {...items[randoNumber]}
+    indexItems.hasItem = false
+    itemsCopy[randoNumber] = indexItems
+    indexItems = {...items[randoNumber]}
+    indexItems.hasItem = true
+    itemsCopy[randoNumber] = indexItems
+    setItems(itemsCopy)
+  }
+
+
+
+  const clickAttempt = () => {
+    setShowItems(!showItems)
+    console.log('lklk')
+  }
+
+
+  let Attempts = 0;
+
+
+  sapperList = items.map((list , index) => (
+      <Sapper key={index}  showItem={showItems} sapperList={clickAttempt}/>
+  ))
+
+
+
   return (
     <div className="App">
+      <div className="sapperTwo">
+        {sapperList}
+      </div>
+      <button onClick={resetSapper}>Reset</button>
     </div>
   );
 }
---
+++ src/components/sapper/Sapper.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Sapper.css'
+interface Props {
+    sapperList: React.MouseEventHandler
+    showItem: boolean
+}
+const Sapper: React.FC<Props> = ({sapperList , showItem}) => {
+    const sapperStyle:  React.CSSProperties = {
+        margin: '1px',
+        width: '40px',
+        height: '40px',
+        background: 'black',
+        display: 'inline-block',
+    }
+
+    if(showItem === true){
+        sapperStyle.background = 'red'
+    }
+
+    return (
+        <div onClick={sapperList} style={sapperStyle} >
+            <span>O</span>
+        </div>
+    );
+};
+
+export default Sapper;
